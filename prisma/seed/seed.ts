6b67e2e4b709e2c3bc5c870123717f04702948b9
Nishadchowdhury <Nishadhj111@gmail.com>
2025-03-12 22:19:16 +0600

    while seeding error is fixed

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,9 +1,14 @@
 import { PrismaClient } from '@prisma/client'
+import { seedTaxonomy } from './taxonomy.seed';
 
 const prisma = new PrismaClient()
 
 async function main() {
-    console.log("test done");
+
+    await prisma.$executeRaw`TRUNCATE TABLE "makes" RESTART IDENTITY CASCADE`; // this command is used to truncate/delete the table and restart the identity column. CASCADE is used to delete the table and all the data in it.
+    // with prisma.$executeRaw we can execute raw sql queries.
+    await seedTaxonomy(prisma);
+
 }
 
 main().catch((e) => {
